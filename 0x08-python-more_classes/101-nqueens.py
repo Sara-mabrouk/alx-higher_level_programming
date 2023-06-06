f30770e91eb51e39ce6dd8e19f9f3aa5a18d2079
Your Name <you@example.com>
2023-06-06 11:14:34 +0300

    N-queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -103,7 +103,6 @@ def recursive_solve(board, row, queens, solutions):
     if queens == len(board):
         solutions.append(get_solution(board))
         return (solutions)
-
     for c in range(len(board)):
         if board[row][c] == " ":
             tmp_board = board_deepcopy(board)
@@ -111,7 +110,6 @@ def recursive_solve(board, row, queens, solutions):
             xout(tmp_board, row, c)
             solutions = recursive_solve(tmp_board, row + 1,
                                         queens + 1, solutions)
-
     return (solutions)
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -123,7 +121,6 @@ if __name__ == "__main__":
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         sys.exit(1)
-
     board = init_board(int(sys.argv[1]))
     solutions = recursive_solve(board, 0, 0, [])
     for sol in solutions:
